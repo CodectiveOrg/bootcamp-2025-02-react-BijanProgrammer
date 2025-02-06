8eceded7afaffa4b918c18083c7b0da2f9365940
BijanProgrammer <bijaneisapour@gmail.com>
2025-02-06 20:27:38 +0330

    [ADD] props example

--- dream-journal/src/App.tsx
+++ dream-journal/src/App.tsx
@@ -3,11 +3,17 @@ import { ReactNode, useState } from "react";
 import "./App.css";
 
 function App(): ReactNode {
+  console.log("rendering app");
+
+  const [parentCount, setParentCount] = useState(0);
+
   return (
     <div>
       <h1>title</h1>
       <p>Lorem ipsum dolor sit amet.</p>
-      <Counter></Counter>
+      <Counter count={parentCount} setCount={setParentCount}>
+        <code>bijan</code>
+      </Counter>
       <ul>
         <li>1</li>
         <li>2</li>
@@ -22,8 +28,14 @@ function App(): ReactNode {
   2. Component should remember variable's previous value (Closure)
  */
 
-function Counter(): ReactNode {
-  const [count, setCount] = useState(0);
+type Props = {
+  children: ReactNode;
+  count: number;
+  setCount: (x: number) => void;
+};
+
+function Counter({ children, count, setCount }: Props): ReactNode {
+  console.log("rendering counter");
 
   const decrement = () => {
     setCount(count - 1);
@@ -35,6 +47,7 @@ function Counter(): ReactNode {
 
   return (
     <div>
+      <h2>Hello, {children}!</h2>
       <div>Count: {count}</div>
       <button onClick={decrement}>Decrement</button>
       <button onClick={increment}>Increment</button>
