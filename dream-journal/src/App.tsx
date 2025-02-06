9d33bfecb215a0e966d94af3ac74d986d5d8ffed
BijanProgrammer <bijaneisapour@gmail.com>
2025-02-06 20:13:32 +0330

    [ADD] why use state

--- dream-journal/src/App.tsx
+++ dream-journal/src/App.tsx
@@ -1,34 +1,44 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { ReactNode, useState } from "react";
+
 import "./App.css";
 
-function App() {
+function App(): ReactNode {
+  return (
+    <div>
+      <h1>title</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <Counter></Counter>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+}
+
+/*
+  1. Tell react variable is changed and you should call the function again
+  2. Component should remember variable's previous value (Closure)
+ */
+
+function Counter(): ReactNode {
   const [count, setCount] = useState(0);
 
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <div>Count: {count}</div>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+    </div>
   );
 }
 
---
+++ scratches/why-use-state.js
@@ -0,0 +1,40 @@
+function generateCounter() {
+  let count = 0;
+
+  const decrement = () => {
+    count--;
+    console.log(`decremented to ${count}`);
+  };
+
+  const increment = () => {
+    count++;
+    console.log(`incremented to ${count}`);
+  };
+
+  const getValue = () => {
+    return count;
+  };
+
+  return [count, decrement, increment, getValue];
+}
+
+function App() {
+  let [count, decrement, increment, getValue] = generateCounter();
+
+  setTimeout(() => {
+    // Interaction
+    increment();
+  }, 1000);
+
+  return count;
+}
+
+// Initial render
+const result1 = App();
+console.log(`result1: ${result1}`);
+
+setTimeout(() => {
+  // Re-render
+  const result2 = App();
+  console.log(`result2: ${result2}`);
+}, 2000);
