b21e792ce0433b93074bf968788dea6f3bf11450
BijanProgrammer <bijaneisapour@gmail.com>
2025-02-09 20:06:33 +0330

    [UPDATE] header's styles

--- dream-journal/src/components/Header/Header.tsx
+++ dream-journal/src/components/Header/Header.tsx
@@ -4,8 +4,8 @@ import style from "./Header.module.css";
 
 function Header(): ReactNode {
   return (
-    <header>
-      <h1 className={style.title}>Dream Journal</h1>
+    <header className={style.header}>
+      <h1>Dream Journal</h1>
     </header>
   );
 }
