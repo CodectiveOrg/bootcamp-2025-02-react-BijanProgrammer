7183a322c1e9c30568f05c6213cef776a05efde0
BijanProgrammer <bijaneisapour@gmail.com>
2025-02-25 21:45:44 +0330

    [ADD] mock api

--- whereabouts/src/providers/AttractionsProvider.tsx
+++ whereabouts/src/providers/AttractionsProvider.tsx
@@ -3,7 +3,6 @@ import {
   ReactElement,
   useContext,
   useEffect,
-  useMemo,
   useState,
 } from "react";
 
@@ -12,36 +11,31 @@ import { FiltersContext } from "../context/filters-context.ts";
 
 import { Attraction } from "../types/attraction.ts";
 
+import { filterAttractions } from "../../api/attractions.ts";
+
 type Props = PropsWithChildren;
 
 function AttractionsProvider({ children }: Props): ReactElement {
   const { filters } = useContext(FiltersContext);
 
-  const [allAttractions, setAllAttractions] = useState<Attraction[]>([]);
-
-  const filteredAttractions = useMemo(() => {
-    return allAttractions.filter((attraction) => {
-      if (filters.tags.length === 0) {
-        return true;
-      }
-
-      return filters.tags.every((tag) =>
-        attraction.tags.find((x) => x.id === tag.id),
-      );
-    });
-  }, [allAttractions, filters]);
+  const [filteredAttractions, setFilteredAttractions] = useState<Attraction[]>(
+    [],
+  );
 
   useEffect(() => {
     const fetchAttractions = async (): Promise<void> => {
-      const response = await fetch(
-        `${import.meta.env.VITE_API_BASE_URL}/attractions`,
-      );
-      const data = await response.json();
-      setAllAttractions(data);
+      // const response = await fetch(
+      //   `${import.meta.env.VITE_API_BASE_URL}/attractions`,
+      // );
+      // const data = await response.json();
+
+      const data = await filterAttractions(filters);
+
+      setFilteredAttractions(data);
     };
 
     fetchAttractions().then();
-  }, []);
+  }, [filters]);
 
   return (
     <AttractionsContext.Provider value={{ filteredAttractions }}>
