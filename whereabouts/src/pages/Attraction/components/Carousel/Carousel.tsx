4952ee122493c2a298082914f2fb2689f3d1e54b
BijanProgrammer <bijaneisapour@gmail.com>
2025-02-27 21:09:03 +0330

    [ADD] carousel functionality

--- whereabouts/src/pages/Attraction/components/Carousel/Carousel.tsx
+++ whereabouts/src/pages/Attraction/components/Carousel/Carousel.tsx
@@ -1,4 +1,6 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
+
+import clsx from "clsx";
 
 import { Attraction } from "../../../../types/attraction.ts";
 
@@ -9,13 +11,43 @@ type Props = {
 };
 
 function Carousel({ attraction }: Props): ReactElement {
+  const [slideIndex, setSlideIndex] = useState<number>(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSlideIndex((old) => (old + 1) % attraction.carousel.length);
+    }, 2000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [attraction]);
+
   return (
     <div className={styles.carousel}>
-      <img
-        src={`${import.meta.env.VITE_CDN_BASE_URL}/${attraction.carousel[0]}`}
-        alt=""
-        height={480}
-      />
+      <ul style={{ transform: `translateX(${slideIndex * 100}%)` }}>
+        {attraction.carousel.map((image) => (
+          <li key={image}>
+            <img
+              src={`${import.meta.env.VITE_CDN_BASE_URL}/${image}`}
+              alt=""
+              height={480}
+            />
+          </li>
+        ))}
+      </ul>
+      <div className={styles.bullets}>
+        {attraction.carousel.map((_, index) => (
+          <button
+            key={index}
+            className={clsx(
+              styles.bullet,
+              index === slideIndex && styles.active,
+            )}
+            onClick={() => setSlideIndex(index)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
