a0eb39f8b6cc6b1a096309f0f4ce509bcf174fea
BijanProgrammer <bijaneisapour@gmail.com>
2025-03-04 20:51:44 +0330

    [REMOVE] manual error

--- whereabouts/src/components/Filters/components/TagFilter/TagFilter.tsx
+++ whereabouts/src/components/Filters/components/TagFilter/TagFilter.tsx
@@ -6,17 +6,8 @@ import { tags } from "../../../../data/tags.ts";
 
 import styles from "./TagFilter.module.css";
 
-let count = 0;
-
 function TagFilter(): ReactElement {
-  console.log("rendering TagFilter");
-
   const { filters, toggleTag } = useContext(FiltersContext);
-
-  if (count <= 4) {
-    count++;
-    throw new Error("Chiz");
-  }
 
   return (
     <div className={styles["tag-filter"]}>
