cf452edf4774b2e1707fde7fc17cf36f41c9ae76
BijanProgrammer <bijaneisapour@gmail.com>
2025-03-04 20:51:19 +0330

    [ADD] error boundary

--- whereabouts/src/components/Filters/Filters.tsx
+++ whereabouts/src/components/Filters/Filters.tsx
@@ -1,5 +1,7 @@
 import { ReactElement } from "react";
 
+import { ErrorBoundary, FallbackProps } from "react-error-boundary";
+
 import TagFilter from "./components/TagFilter/TagFilter.tsx";
 
 import styles from "./Filters.module.css";
@@ -7,7 +9,19 @@ import styles from "./Filters.module.css";
 function Filters(): ReactElement {
   return (
     <div className={styles.filters}>
-      <TagFilter />
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <TagFilter />
+      </ErrorBoundary>
+    </div>
+  );
+}
+
+function ErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
+  return (
+    <div role="alert">
+      <p>خطای غیرمنتظره:</p>
+      <pre style={{ color: "red" }}>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>تلاش مجدد</button>
     </div>
   );
 }
