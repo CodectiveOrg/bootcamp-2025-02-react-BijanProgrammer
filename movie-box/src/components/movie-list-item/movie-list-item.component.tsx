8e36e386b01acb6f688a5b1e4bc74ae5bf87eb96
BijanProgrammer <bijaneisapour@gmail.com>
2025-03-06 20:29:44 +0330

    [ADD] custom hook

--- movie-box/src/components/movie-list-item/movie-list-item.component.tsx
+++ movie-box/src/components/movie-list-item/movie-list-item.component.tsx
@@ -2,13 +2,11 @@ import { ReactElement, useMemo } from "react";
 
 import { Link } from "react-router";
 
-import { useQuery } from "@tanstack/react-query";
-
 import clsx from "clsx";
 
-import { fetchGenresApi } from "../../api/fetch-genres.api.ts";
-
 import FluentEmojiStar from "../../icons/FluentEmojiStar.tsx";
+
+import useGenresQuery from "../../queries/use-genres.query.ts";
 
 import { MovieListItemType } from "../../types/movie-list-item.type.ts";
 
@@ -19,10 +17,7 @@ type Props = {
 };
 
 function MovieListItemComponent({ movie }: Props): ReactElement {
-  const { data: allGenres } = useQuery({
-    queryKey: ["genres"],
-    queryFn: fetchGenresApi,
-  });
+  const { data: allGenres } = useGenresQuery();
 
   const movieGenres = useMemo(() => {
     if (!allGenres) {
