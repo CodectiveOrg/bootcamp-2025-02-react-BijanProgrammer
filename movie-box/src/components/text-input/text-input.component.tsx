aedd30e9edbb7c620b593d0a6af9e639823c7bc1
BijanProgrammer <bijaneisapour@gmail.com>
2025-03-11 12:51:44 +0330

    [ADD] validation errors

--- movie-box/src/components/text-input/text-input.component.tsx
+++ movie-box/src/components/text-input/text-input.component.tsx
@@ -2,34 +2,37 @@ import { ComponentProps, ReactElement, forwardRef, ForwardedRef } from "react";
 
 import clsx from "clsx";
 
+import ErrorsComponent from "./components/errors/errors.component.tsx";
+import IconButtonComponent from "./components/icon-button/icon-button.component.tsx";
+
 import styles from "./text-input.module.css";
 
 type Props = ComponentProps<"input"> & {
   label: string;
   suffixIcon?: ReactElement;
   onSuffixClick?: () => void;
+  errors?: string[];
 };
 
 function TextInputComponent(
-  { label, suffixIcon, onSuffixClick, className, ...otherProps }: Props,
+  { label, suffixIcon, onSuffixClick, errors, className, ...otherProps }: Props,
   ref: ForwardedRef<HTMLInputElement>,
 ): ReactElement {
   return (
-    <label className={clsx(styles["text-input"], className)}>
-      <div className={styles.title}>{label}</div>
-      <div className={styles.box}>
-        <input ref={ref} type="text" placeholder="" {...otherProps} />
-        {suffixIcon && (
-          <button
-            type="button"
-            className={styles.suffix}
-            onClick={onSuffixClick}
-          >
-            {suffixIcon}
-          </button>
-        )}
-      </div>
-    </label>
+    <div className={clsx(styles["text-input"], className)}>
+      <label className={clsx(errors && errors.length > 0 && styles.invalid)}>
+        <div className={styles.title}>{label}</div>
+        <div className={styles.box}>
+          <input ref={ref} type="text" placeholder="" {...otherProps} />
+          {suffixIcon && (
+            <IconButtonComponent onClick={onSuffixClick}>
+              {suffixIcon}
+            </IconButtonComponent>
+          )}
+        </div>
+      </label>
+      <ErrorsComponent className={styles.errors} errors={errors} />
+    </div>
   );
 }
 
