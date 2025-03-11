3f08736bb090b53f404e5182029401a26fe59ac1
BijanProgrammer <bijaneisapour@gmail.com>
2025-03-11 12:45:58 +0330

    [ADD] sign up page's api connection

--- movie-box/src/pages/auth/components/sign-up-form/sign-up-form.component.tsx
+++ movie-box/src/pages/auth/components/sign-up-form/sign-up-form.component.tsx
@@ -1,20 +1,56 @@
-import { ReactElement } from "react";
+import { FormEvent, ReactElement } from "react";
+
+import { useMutation } from "@tanstack/react-query";
 
 import { Link } from "react-router";
 
+import { fetchSignUpApi } from "../../../../api/fetch-sign-up.api.ts";
+
+import ButtonComponent from "../../../../components/button/button.component.tsx";
 import TextInputComponent from "../../../../components/text-input/text-input.component.tsx";
 import PasswordInputComponent from "../../../../components/password-input/password-input.component.tsx";
 
+import { SignUpDto } from "../../../../dto/sign-up.dto.ts";
+
 import styles from "./sign-up-form.module.css";
-import ButtonComponent from "../../../../components/button/button.component.tsx";
 
 export default function SignUpFormComponent(): ReactElement {
+  const mutation = useMutation({
+    mutationFn: fetchSignUpApi,
+  });
+
+  const formSubmitHandler = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const dto: SignUpDto = {
+      username: formData.get("username") as string,
+      password: formData.get("password") as string,
+    };
+
+    mutation.mutate(dto, {
+      onSuccess: (result) => {
+        if ("error" in result) {
+          console.log(result.validationErrors);
+          console.log("error");
+        } else {
+          console.log("success");
+        }
+      },
+    });
+  };
+
   return (
     <div className={styles["sign-up-form"]}>
       <h1>Sign Up!</h1>
-      <form>
-        <TextInputComponent label="Username" />
-        <PasswordInputComponent label="Password" autoComplete="new-password" />
+      <form onSubmit={formSubmitHandler}>
+        <TextInputComponent label="Username" name="username" />
+        <PasswordInputComponent
+          label="Password"
+          name="password"
+          autoComplete="new-password"
+        />
         <ButtonComponent>Sign Up</ButtonComponent>
       </form>
       <div className={styles["change-form"]}>
