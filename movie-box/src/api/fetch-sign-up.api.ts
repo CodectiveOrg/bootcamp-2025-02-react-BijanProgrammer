e8ea0b53165516ab405173c3321195c8b70498b2
BijanProgrammer <bijaneisapour@gmail.com>
2025-03-11 09:45:03 +0330

    [ADD] fetch sign up api

---
+++ movie-box/src/api/fetch-sign-up.api.ts
@@ -0,0 +1,17 @@
+import { ResponseDto } from "../dto/response.dto.ts";
+import { SignUpDto } from "../dto/sign-up.dto.ts";
+
+export async function fetchSignUpApi(dto: SignUpDto): Promise<ResponseDto> {
+  const response = await fetch(
+    `${import.meta.env.VITE_API_BASE_URL}/auth/sign-up`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dto),
+    },
+  );
+
+  return await response.json();
+}
