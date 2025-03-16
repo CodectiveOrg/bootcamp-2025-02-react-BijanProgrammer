8a17e30113d7d44273a6e61320c944a2d373a9bc
BijanProgrammer <bijaneisapour@gmail.com>
2025-03-16 15:35:04 +0330

    [REFACTOR] filters using zustand

---
+++ movie-box/src/stores/filters.store.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+
+import { GenreType } from "../types/genre.type.ts";
+import { FiltersType } from "../types/filters.type.ts";
+
+type FiltersStore = {
+  filters: FiltersType;
+  toggleGenre: (genre: GenreType) => void;
+};
+
+export const useFiltersStore = create<FiltersStore>((set) => ({
+  filters: {
+    genres: [],
+  },
+  toggleGenre: (genre: GenreType) => {
+    set((old) => {
+      const index = old.filters.genres.findIndex((x) => x.id === genre.id);
+
+      if (index === -1) {
+        return {
+          filters: {
+            ...old.filters,
+            genres: [...old.filters.genres, genre],
+          },
+        };
+      }
+
+      const clone = [...old.filters.genres];
+      clone.splice(index, 1);
+      return { filters: { ...old.filters, genres: clone } };
+    });
+  },
+}));
