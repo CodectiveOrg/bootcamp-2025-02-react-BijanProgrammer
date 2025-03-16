1df70df27dd5a8434a193a32410fab922c2e79f7
BijanProgrammer <bijaneisapour@gmail.com>
2025-03-16 20:44:00 +0330

    [ADD] client-side validation

--- movie-box/src/components/text-input/text-input.component.tsx
+++ movie-box/src/components/text-input/text-input.component.tsx
@@ -1,5 +1,7 @@
 import { ComponentProps, ReactElement } from "react";
 
+import { FieldError } from "react-hook-form";
+
 import clsx from "clsx";
 
 import IconButtonComponent from "./components/icon-button/icon-button.component.tsx";
@@ -11,6 +13,7 @@ type Props = ComponentProps<"input"> & {
   label: string;
   suffixIcon?: ReactElement;
   onSuffixClick?: () => void;
+  clientError?: FieldError;
   serverErrors?: string[];
 };
 
@@ -18,17 +21,16 @@ export default function TextInputComponent({
   label,
   suffixIcon,
   onSuffixClick,
+  clientError,
   serverErrors,
   className,
   ...otherProps
 }: Props): ReactElement {
+  const isInvalid = clientError || (serverErrors && serverErrors.length > 0);
+
   return (
     <div className={clsx(styles["text-input"], className)}>
-      <label
-        className={clsx(
-          serverErrors && serverErrors.length > 0 && styles.invalid,
-        )}
-      >
+      <label className={clsx(isInvalid && styles.invalid)}>
         <div className={styles.title}>{label}</div>
         <div className={styles.box}>
           <input type="text" placeholder="" {...otherProps} />
@@ -39,6 +41,7 @@ export default function TextInputComponent({
           )}
         </div>
       </label>
+      <div className={styles["client-error"]}>{clientError?.message}</div>
       <ServerErrorsComponent
         className={styles["server-errors"]}
         serverErrors={serverErrors}
